9f7a08592525a7923a098b64b43bdc7583d7cfaf
UgoCastellazzi <61455340+UgoCastellazzi@users.noreply.github.com>
2020-06-24 18:34:40 +0200

    Update fetch_tweets_job.rb

--- app/jobs/fetch_tweets_job.rb
+++ app/jobs/fetch_tweets_job.rb
@@ -1,9 +1,5 @@
 require 'twitter'
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "zXdK9dgaSkUihiUzVLehOPh3y"
-  config.consumer_secret     = "TcdmSoDr3fMTnCtUluGrFBh7k9l0zlWJNEf932mvCpjK5LIiiJ"
-  config.access_token        = "1565624988-kNPG9im0aaZ0mi5jJf78Q0xaoieWFxe7UBueYI2"
-  config.access_token_secret = "ctUO3Zv7pbAAot7VUOIuN5PFaGx0s4g666eV2oXAPHxbF"
 end
 
 def perform(keyword)
