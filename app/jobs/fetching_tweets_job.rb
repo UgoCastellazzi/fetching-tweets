a36f0b304690c2c669f87f01ec41aebc0e5fc0c1
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-25 12:55:34 +0200

    keyword routes ok, refresh button ok, limit tweets to accounts > 5k followers

--- app/jobs/fetching_tweets_job.rb
+++ app/jobs/fetching_tweets_job.rb
@@ -12,14 +12,20 @@ class FetchingTweetsJob < ApplicationJob
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
-    client.search("#{keyword.title}", result_type: "recent").take(1).collect do |tweet|
-      Tweet.create!(
-        handdle: tweet.user.screen_name,
-        content: tweet.text,
-        date: tweet.created_at,
-        link: "https://twitter.com/i/web/status/#{tweet.id}",
-        keyword_id: keyword.id
-      )
+    client.search("#{keyword.title} -rt -RT", result_type: "recent").take(10).collect do |tweet|
+      if Tweet.exists?(link: "https://twitter.com/i/web/status/#{tweet.id}")
+        return
+      else
+        if tweet.user.followers_count > 5000
+          Tweet.create!(
+            handdle: tweet.user.screen_name,
+            content: tweet.text,
+            date: tweet.created_at,
+            link: "https://twitter.com/i/web/status/#{tweet.id}",
+            keyword_id: keyword.id
+          )
+        end
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :keywords, only: [:create, :new, :index, :destroy]
+  resources :keywords, only: [:create, :new, :index, :destroy] do
+    member do
+      get :tweets
+      get :refresh_tweets
+    end
+  end
 end
